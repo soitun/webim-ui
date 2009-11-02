06637c8f46e2c10d72936789215c510279e7e6a1
hidden <zzdhidden@gmail.com>
2009-11-02 23:21:10 +0800

    Create webim-zh-TW.js

--- ui/js/i18n/webim-zh-TW.js
+++ ui/js/i18n/webim-zh-TW.js
@@ -1,5 +1,5 @@
-﻿webim.ui.i18n.locale = 'zh-TW';
-webim.ui.i18n.store('zh-TW',{
+webim.ui.i18n.locale = 'zh-CN';
+webim.ui.i18n.store('zh-CN',{
         "dt:monthdate": "{{month}}{{date}}日",
         "dt:today": "今天",
         "dt:yesterday": "昨天",
@@ -24,20 +24,51 @@ webim.ui.i18n.store('zh-TW',{
         "dt:sunday": "星期天"
     });
     	
-webim.ui.i18n.store('zh-TW',{
-	"new message":"有新消息",
+webim.ui.i18n.store('zh-CN',{
 	"close":"關閉",
 	"minimize":"最小化",
-	"search friend":"搜索好友",
-	"no friend":"沒有在線好友",
-	"group":"群組",
-	"input notice":"這裏輸入消息，Enter鍵發送",
-	"clear history":"清除聊天歷史記錄",
-	"emot":"表情",
-	"notavail":"不在線! 不能發送消息",
+	"maximize":"最大化",
+	"restore":"還原",
+        "setting":"設置",
+        "notification":"通知",
+	"hotpost":"熱帖",
+        "chat":"聊天",
+        "menu":"工具欄",
+        "online":"上線",
+        "connect":"連接",
+        "connecting":"連接中",
+        "offline":"離線",
+        "search buddy": "搜索聯系人",
+        "user offline notice": "您處於離線狀態，不能收到消息",
+        "buddy offline notice": "{{name}}已離線，您所發送的消息{{name}}將在下次登錄時收到",
+        "input notice": "這裏輸入消息，Enter鍵發送",
+        "clear history notice": "聊天歷史記錄已清除",
+	"typing":" 正在輸入...",
 	"inputing":" 正在輸入...",
-	"leave-channel":" 離開群組聊天...",
-	"join-channel":" 加入群組聊天...",
-	"people online":"人在線",
-	"group-notavail":"加入群聊失敗，請關閉此聊天窗口重新打開。"
+	"chat with":"和{{name}}聊天",
+	"chat with me":"和我聊天",
+	"new message":"有新消息",
+        "connect error":"連接失敗",
+        "disconnect":"連接斷開",
+        "online error":"登錄失敗",
+        "play_sound": "收到新消息時播放提示音",
+        "buddy_sticky": "保持聊天窗口始終打開",
+        "msg_auto_pop": "新消息時自動彈出聊天窗口",
+        "minimize_layout": "收縮工具條",
+	"emot":"表情",
+	"clear history":"清除歷史記錄",
+        "empty buddy": "沒有好友在線",
+        "empty menu": "沒有工具",
+        "empty notification": "空",
+        "collapse": "收縮",
+        "expand": "展開",
+	"friend":"好友",
+	"stranger":"陌生人"
+});
+webim.ui.i18n.store('zh-CN',{
+	"doing": "記錄",
+	"album": "相冊",
+	"blog": "日誌",
+	"thread": "群組",
+	"share": "分享"
 });

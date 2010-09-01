5eed6eafc1c713c84a011f651f47844c24049705
Hidden <zzdhidden@gmail.com>
2010-09-01 20:25:32 +0800

    Chatlink in space

--- build/uchome.mk
+++ build/uchome.mk
@@ -7,5 +7,6 @@ JS_APP_FILES = ${APP_SRC_DIR}/ui.chatlink.js\
 
 CSS_APP_FILES = ${APP_SRC_DIR}/chatlink.css\
 	${APP_SRC_DIR}/notification.css\
+	${APP_SRC_DIR}/uchome.css\
 
 include build/include.mk

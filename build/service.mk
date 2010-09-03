9126bfdc876334705ca8de005752c689e94ad0c5
Hidden <zzdhidden@gmail.com>
2010-09-04 05:06:55 +0800

    Chat subheader

--- build/service.mk
+++ build/service.mk
@@ -5,6 +5,7 @@ PRODUCT_NAME = service
 JS_APP_FILES = ${JS_SRC_DIR}/ui.buddy.js\
 
 CSS_APP_FILES = ${CSS_SRC_DIR}/buddy.css\
+	${CSS_SRC_DIR}/user.css\
 	${APP_SRC_DIR}/service.css\
 
 include build/include.mk

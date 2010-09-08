b947b7ca34318b109697472cb4122700b823559c
Hidden <zzdhidden@gmail.com>
2010-09-08 12:53:31 +0800

    Add function stripHTML

--- test/unit/core.js
+++ test/unit/core.js
@@ -13,5 +13,11 @@ test("webim.ui Class", 4, function() {
 	ok(hasClass(el, "ui-state-default"), "addClass");
 	replaceClass(el, "ui-state-default", "ui-state-hightlight");
 	ok(!hasClass(el, "ui-state-default"), "replaceClass");
-	
+
 });
+
+test("Function stripHTML", 2, function() {
+	equal(stripHTML('<img src="b.png" />dd<img src="b.png" />'), 'dd', "remove img tag")
+	equal(stripHTML('<a href="b.png" >bb</a>dd'), 'bbdd', "remove a tag")
+});
+
